0de16a6c283bb81f8789ccf6d1ca6216e12c7c71
Santiago Dimaren <sdimaren1@gmail.com>
2023-11-29 22:58:07 +0000

    routing fix (#34)
    Reviewed-on: https://codeberg.org/reality-hack-inc/realityhack.world-frontend/pulls/34
    Co-authored-by: Santiago Dimaren <sdimaren1@gmail.com>
    Co-committed-by: Santiago Dimaren <sdimaren1@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,8 @@
 'use client';
 import { useSession } from 'next-auth/react';
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (status === 'loading') return;
-
-    if (!session && pathname !== '/apply' && pathname !== '/signin') {
-      router.replace('/apply');
-      return;
-    }
-
-    if (session) {
-      router.replace('/');
-    }
-  }, []);
 
   return (
     <div className="h-screen p-6">
