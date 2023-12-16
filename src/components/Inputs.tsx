8ed97905a660c644d882c33867a5e285a6c0930a
Santiago Dimaren <sdimaren1@gmail.com>
2023-12-16 13:11:33 -0500

    update email regex to support longer tlds

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -20,7 +20,7 @@ export const validateField = (
   // Field-specific validations
   switch (type) {
     case 'email':
-      const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
       if (!emailPattern.test(value)) {
         return 'Invalid email format.';
       }
