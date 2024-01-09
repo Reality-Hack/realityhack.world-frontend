222388181addb8cdcac541caf9c12514e999cff5
Au613 <Austinmit613@gmail.com>
2024-01-09 09:05:39 -0500

    adding api endpoints for the workshop schedules

---
+++ src/app/api/workshops.ts
@@ -0,0 +1,41 @@
+//helpful files - applications.ts file - for other api calls | ApplicationTable.tsx for frontend component calling the api
+//helpful links - devDB - https://dev-api.realityhack.world/schema/swagger/ | https://dev-api.realityhack.world/workshops/
+
+
+//USER SCHEDULE FOR WORKSHOP
+
+export async function getAllWorkshops(accessToken:string) {
+
+}
+
+//GET USER'S SPECIFIC WORKSHOP SCHEDULE
+export async function getMyWorkshops() {
+    
+}
+
+//UPDATE A USER'S SPECIFIC WORKSHOP SCHEDULE
+export async function updateMyWorkshops() {
+
+}
+
+
+// take attendance for workshop (user signs in to a specific workshop)
+export async function signinToWorkshop() {
+
+}
+
+//ADMIN SIDE 
+
+//take in a list of workshops and adds to the work
+export async function updateWorkshopSchedule() {
+
+}
+
+export async function deleteWorkshopFromSchedule() {
+
+}
+
+//update a single workshop's info
+export async function updateSpecificWorkshop() {
+
+}
