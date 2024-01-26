f644ab357af17c547bef9151c20a75f4288157cd
Au613 <Austinmit613@gmail.com>
2024-01-26 04:17:10 +0000

    adding sponsor table to admin rsvp space (#73)
    Co-authored-by: Peter Iordanov <piordanov@noreply.codeberg.org>
    Reviewed-on: https://codeberg.org/reality-hack-inc/realityhack.world-frontend/pulls/73
    Reviewed-by: Peter Iordanov <piordanov@noreply.codeberg.org>
    Co-authored-by: Au613 <Austinmit613@gmail.com>
    Co-committed-by: Au613 <Austinmit613@gmail.com>

---
+++ src/app/(dashboard)/admin/rsvp/sponsors/page.tsx
@@ -0,0 +1,9 @@
+import RSVPTable from '@/components/admin/RSVPTable';
+
+export default async function Sponsors() {
+  return (
+    <div className="pb-8 ">
+      <RSVPTable type="S" />
+    </div>
+  );
+}
