358d9c94f3e2d30a7898338c237f474050beed91
Albert Schweitzer <schweitzer.albert@gmail.com>
2024-11-25 00:05:09 +0000

    Basic Participant Team Page (#99)
    Co-authored-by: pmelga01 <pmelga01@noreply.codeberg.org>
    Reviewed-on: https://codeberg.org/reality-hack-inc/realityhack.world-frontend/pulls/99
    Co-authored-by: Albert Schweitzer <schweitzer.albert@gmail.com>
    Co-committed-by: Albert Schweitzer <schweitzer.albert@gmail.com>

--- src/app/(dashboard)/team/page.tsx
+++ src/app/(dashboard)/team/page.tsx
@@ -1,14 +1,42 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import { useCallback, useEffect, useState } from 'react';
+import { SerializedTeam, getTeam } from '@/app/api/team';
+import { useAuthContext } from '@/hooks/AuthContext';
 
 export default function Team() {
   const { data: session, status } = useSession();
+  const [team, setTeam] = useState<SerializedTeam>();
+
+  const { user } = useAuthContext();
+  console.log(user);
+
+  useEffect(() => {
+    if (session?.access_token && user?.team?.id) {
+      getTeam(user.team.id, session.access_token).then(result => {
+        setTeam(result);
+      });
+    }
+  }, [user]);
+
+  console.log(team?.table);
 
   return (
-    <div className="h-screen p-6">
-      <>
-        <h1>Teams</h1>
-      </>
+    <div className="h-screen p-6 flex flex-row justify-center items-start">
+      <div className="grid grid-cols-2 gap-4 mx-8 mt-12">
+          <h2 className="text-lg font-bold">Team Name: </h2>
+          <div className="text-lg">{team?.name}</div>
+          <h3 className="text-lg font-bold">Team Members:</h3>
+          <ul>
+            {team?.attendees.map((attendee) => (
+          <li key={attendee.id}>{attendee.first_name} {attendee.last_name}</li>
+            ))}
+          </ul>
+          <p className="text-lg font-bold">Table Number: </p>
+          <div className="text-lg">{team?.table?.number}</div>
+          <p className="text-lg font-bold">Table Location: </p>
+          <p className="text-lg">Building {team?.table?.location?.building}, Room {team?.table?.location?.room}</p>
+      </div>
     </div>
   );
 }
